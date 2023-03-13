6eb9b3f99bb62ba0158c417623f0e98cf9a82ee2
Vasco Santos <santos.vasco10@gmail.com>
2023-03-13 16:50:56 +0100

    fix: denylist needs to move to other package (#150)
    We moved KV to denylist a while back, but we were still having it in
    binding.
    
    I removed last week because release wass failing
    https://github.com/web3-storage/reads/commit/9bac59d0e7b0f11fd1364c3127c64ffc6bd7d0f5

--- packages/denylist/scripts/cli.js
+++ packages/denylist/scripts/cli.js
@@ -3,6 +3,7 @@
 import sade from 'sade'
 
 import { buildCmd } from './build.js'
+import { denylistSyncCmd, denylistAddCmd } from './denylist.js'
 
 const env = process.env.ENV || 'dev'
 const prog = sade('denylist')
@@ -12,5 +13,16 @@ prog
   .describe('Build the worker.')
   .option('--env', 'Environment', env)
   .action(buildCmd)
+  .command('denylist sync')
+  .describe('Sync the gateway deny list with various sources.')
+  .option('--env', 'Wrangler environment to use.', env)
+  .action(denylistSyncCmd)
+  .command('denylist add <cid>')
+  .describe(
+    'Add a CID (or CID + path) to the local deny list. Note: we currently DO NOT support denying by CID + path in the API.'
+  )
+  .option('--status', 'HTTP status to send in response.')
+  .option('--reason', 'Reason for deny. Note: may be communicated in response')
+  .action(denylistAddCmd)
 
 prog.parse(process.argv)
--- packages/edge-gateway/scripts/cli.js
+++ packages/edge-gateway/scripts/cli.js
@@ -4,7 +4,6 @@ import sade from 'sade'
 
 import { buildCmd } from './build.js'
 import { ipfsCmd } from './ipfs.js'
-import { denylistSyncCmd, denylistAddCmd } from './denylist.js'
 import { heartbeatCmd } from './heartbeat.js'
 
 const env = process.env.ENV || 'dev'
@@ -24,16 +23,5 @@ prog
   .option('--token', 'Opsgenie Token')
   .option('--name', 'Heartbeat Name')
   .action(heartbeatCmd)
-  .command('denylist sync')
-  .describe('Sync the gateway deny list with various sources.')
-  .option('--env', 'Wrangler environment to use.', env)
-  .action(denylistSyncCmd)
-  .command('denylist add <cid>')
-  .describe(
-    'Add a CID (or CID + path) to the local deny list. Note: we currently DO NOT support denying by CID + path in the API.'
-  )
-  .option('--status', 'HTTP status to send in response.')
-  .option('--reason', 'Reason for deny. Note: may be communicated in response')
-  .action(denylistAddCmd)
 
 prog.parse(process.argv)
