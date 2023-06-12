fc8b9f2cf823d7433d99e49fe5e56991fbf222e0
Alan Shaw <alan.shaw@protocol.ai>
2023-06-12 16:28:40 +0100

    feat(denylist): edge cached responses (#162)
    * Cache responses for listed CIDs for 1 day
    * Cache responses for unlisted CIDs for 1 hour
    
    ---------
    
    Co-authored-by: Vasco Santos <santos.vasco10@gmail.com>

---
+++ packages/denylist/src/cache.js
@@ -0,0 +1,37 @@
+/* eslint-env browser */
+/**
+ * Intercepts request if content cached by just returning cached response.
+ * Otherwise proceeds to handler.
+ * @param {import('itty-router').RouteHandler<Request>} handler
+ */
+export function withCdnCache (handler) {
+  /**
+   * @param {Request} request
+   * @param {import('./env').Env} env
+   * @param {ExecutionContext} ctx
+   */
+  return async (request, env, ctx) => {
+    // Should skip cache if instructed by headers
+    if ((request.headers.get('Cache-Control') || '').includes('no-cache')) {
+      return handler(request, env, ctx)
+    }
+
+    let response
+    // Get from cache and return if existent
+    /** @type {Cache} */
+    const cache = caches.default
+    const cacheKey = new Request(request.url)
+    response = await cache.match(cacheKey)
+    if (response) return response
+
+    // If not cached and request wants it _only_ if it is cached, send 412
+    if (request.headers.get('Cache-Control') === 'only-if-cached') {
+      return new Response(null, { status: 412 })
+    }
+
+    response = await handler(request, env, ctx)
+    ctx.waitUntil(cache.put(cacheKey, response.clone()))
+
+    return response
+  }
+}
